560fd973faefe0da9666a855357777cbe44a739d
Ben DeVries <bjd47@students.calvin.edu>
2022-02-04 15:38:22 -0500

    initial deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC37vlxM9DVgLzKJ5IXtal1MF8s8gVWzgQ",
+    authDomain: "calvin-soda-pop.firebaseapp.com",
+    projectId: "calvin-soda-pop",
+    storageBucket: "calvin-soda-pop.appspot.com",
+    messagingSenderId: "210960832131",
+    appId: "1:210960832131:web:b600407f099bf539476db6",
+    measurementId: "G-BQ5657VXMQ"
+  }
 };
